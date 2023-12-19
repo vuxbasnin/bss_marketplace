82063adddb143e946d2a87fb10221c1a53405743
Nin <ninvuba@vccorp.vn>
2023-12-19 17:04:50 +0700

    [update] Grid in tab Home

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,8 +1,25 @@
 import React, { useState, useEffect, useCallback, useMemo } from 'react';
-import { Container } from '@material-ui/core';
+import { Box, Container } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
 export default function Home() {
     return (
-        <div></div>
+        <Container maxWidth="lg" style={{ marginTop: '64px' }}>
+            <Grid container>
+                <Grid item xs={12} sm={6} md={3}>
+                    <Paper>1</Paper>
+                </Grid>
+                <Grid item xs={12} sm={6} md={3}>
+                    <Paper>2</Paper>
+                </Grid>
+                <Grid item xs={12} sm={6} md={3}>
+                    <Paper>3</Paper>
+                </Grid>
+                <Grid item xs={12} sm={6} md={3}>
+                    <Paper>4</Paper>
+                </Grid>
+            </Grid>
+        </Container>
     );
 }
